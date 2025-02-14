6686ceae8ab9c855ddd3ec91d667aa682c451590
David Salman <d.salman777@gmail.com>
2025-02-14 13:20:31 -0500

    feat: add docstrings to rtk-mock-store createMockStore factory function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
 import { createAsyncThunk, ReducersMapObject, Action, ThunkAction } from '@reduxjs/toolkit'
 
+/**
+ * Action type defintions for actions dispatched from createAsyncThunk.
+ * Need to stub the types here to properly check the action type in mock
+ * store's runThunk function to ensure complete type safety.
+ */
 type CreateAsyncThunkAction = ReturnType<typeof createAsyncThunk>
 type AsyncThunkAction = ReturnType<CreateAsyncThunkAction>
 
+/**
+ * Data structures and methods of a mock store.
+ *
+ * @typeParam MockRootState The root state of the mock store.
+ *
+ * @property rootStateSnapshots         The snapshots of the root state of the mock store.
+ * @property rootState                  The root state of the mock store.
+ * @property extraArgument              The extra argument of the mock store.
+ * @property getRootState               Returns the root state of the mock store.
+ * @property getReducerState            Returns the state of the reducer being tested.
+ * @property setRootState               Sets the root state of the mock store.
+ * @property setReducerState            Sets the state of the reducer being tested.
+ * @property getRootStateSnapshots      Returns the snapshots of the root state of the mock store.
+ * @property getRootStateSnapshot       Returns the snapshot of the root state of the mock store for a specific action type.
+ * @property getReducerStateSnapshots   Returns the snapshots of the reducer being tested.
+ * @property getReducerStateSnapshot    Returns the snapshot of the reducer being tested for a specific action type.
+ * @property printReducerStateSnapshots Prints the snapshots of the reducer being tested.
+ * @property printRootStateSnapshots    Prints the snapshots of the root state of the mock store.
+ * @property next                       The next middleware function.
+ * @property runThunk                   Runs a thunk action.
+ * @property getState                   Returns the root state of the mock store.
+ * @property dispatch                   Dispatches an action to the mock store.
+ * @property reset                      Resets the mock store state to intialRootState.
+ */
 type RTKMockStore<MockRootState> = {
   rootStateSnapshots: Record<Action['type'], MockRootState>[]
   rootState: MockRootState
@@ -26,11 +55,54 @@ type RTKMockStore<MockRootState> = {
   reset: () => void
 }
 
+/**
+ * Creates a mock store for testing redux actions and reducers using redux-toolkit.
+ * Can be used to test a single reducer in isolation or multiple reducers together.
+ *
+ * Note: Typings for root state and actions are inferred if using redux-toolkit slices,
+ * so its strongly recommended to use redux-toolkit slices for reducers and actions.
+ *
+ * @typeParam MockRootState The root state of the mock store.
+ *
+ * @param reducerToTest     The reducer to test.
+ * @param reducers          All reducers in the store.
+ * @param initialRootState  The initial state of the store.
+ *
+ * @returns A mock store.
+ *
+ * @example
+ * // __tests__/reducer.test.ts
+ * import createMockStore from 'rtk-mock-store';
+ * import { INITIAL_STATE, reducer, testAction } from './reducer';
+ *
+ * const mockStore = createMockStore(
+ *   'reducerToTest',
+ *   { reducerToTest: reducer },
+ *   { reducerToTest: INITIAL_STATE },
+ * );
+ *
+ * describe('reducerToTest', () => {
+ *   afterEach(() => {
+ *     mockStore.reset();
+ *   });
+ *
+ *   it('should have initial state', () => {
+ *     expect(mockStore.getReducerState()).toEqual({ test: false });
+ *   });
+ *   it('should set test to true', () => {
+ *     mockStore.dispatch(testAction());
+ *     expect(mockStore.dispatch.mock.calls[0][0].type).toEqual('reducerToTest/TEST_ACTION');
+ *     expect(mockStore.getReducerState()).toEqual({ test: true });
+ *   });
+ * });
+ */
 export default function createMockStore<MockRootState extends object>(
   reducerToTest: keyof MockRootState,
   reducers: ReducersMapObject<MockRootState, Action>,
   initialRootState: MockRootState,
 ): RTKMockStore<MockRootState> {
+  /* Middleware Types */
+
   type ThunkMiddlewareAction = ThunkAction<unknown, MockRootState, {}, Action>
   type ThunkMiddlewareProps = {
     dispatch: (action: ThunkMiddlewareAction) => void
@@ -38,6 +110,8 @@ export default function createMockStore<MockRootState extends object>(
     extraArgument: {}
   }
   type NextAction = (action: Action) => void
+
+  /* Middleware Mock */
 
   const thunkMiddleware =
     ({ dispatch, getState }: ThunkMiddlewareProps) =>
@@ -48,6 +122,8 @@ export default function createMockStore<MockRootState extends object>(
       }
       return next(action)
     }
+
+  /* Store Mock */
 
   const store: RTKMockStore<MockRootState> = {
     rootState: { ...initialRootState },
